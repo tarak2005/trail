d2adc5acf7752e63a21c0171602c4573d6662864
tarak2005 <66374522+tarak2005@users.noreply.github.com>
2020-08-23 13:50:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ img = loadImage("image.png")
 
 function setup(){
 //create a canvas
-canvas = createCanvas(400,windowHeight); 
+canvas = createCanvas(400,700); 
 database = firebase.database();
 
 //form = new Form();
